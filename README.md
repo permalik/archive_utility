78a2beb3ff43c6f745894e91e552b118b32ee810
permalik <permalik@protonmail.com>
2024-04-15 19:38:30 -0500

    reset

--- README.md
+++
@@ -1,1 +0,0 @@
-Utilities, Tools, and Jobs.
--- src/routes/api/repos/+server.ts
+++
@@ -1,7 +0,0 @@
-import { RepoService } from '../../../lib/services';
-
-export async function POST() {
-	const repoService = new RepoService();
-	await repoService.refreshRepos();
-	return new Response();
-}
